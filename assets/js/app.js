b077f861e2f9ab95901daba2d857b50648a449aa
80s Action Hero <alex.stanley89@gmail.com>
2018-12-08 23:53:32 -0800

    graph shows up

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,10 @@ var svgHeight = 500;
 
 // define object for chart margins
 var margins = {
-    top: 60,
-    right: 60,
-    bottom: 60,
-    left: 60
+    top: 100,
+    right: 100,
+    bottom: 100,
+    left: 100
 };
 
 // def demensions of the chart
@@ -23,50 +23,35 @@ var svg = d3.select('#scatter')
 
 // append a group area and set margins
 var chartGroup = svg.append('g')
-    .attr('transfrom', `translate(${margin.left}, ${margin.top})`);
+    .attr('transfrom', `translate(${margins.left}, ${margins.top})`);
 
 // Read data from csv
-d3.csv('/data/data.csv', function(error, healthData) {
+d3.csv('assets/data/data.csv').then(function(healthData) {
     // Error handling
-    if (error) throw(error);
+    // if (error) throw(error);
 
     // Verify data in the console 
     console.log(healthData);
 
-    // Create a list of state + abbrs
-    var states = healthData.map(data => [data.state, data.abbr]);
-
-    // Verify states data in console
-    console.log('State Names and abbr:');
-    states.forEach(state => {
-        console.log(`Full: ${state[0]}, Abbr: ${state[1]}`);
-    })
-
-    // Cast the relevant data to a number. Interested in Healthcare vs Poverty
     healthData.forEach(element => {
-        element.healthcare = +element.healthcare;
+        element.noHealthInsurance = +element.noHealthInsurance;
         element.poverty = +element.poverty;
     });
 
-    // max and min values for poverty data
-    var povertyLim = [
-        d3.min(healthData, d => d.poverty),
-        d3.max(healthData, d=> d.povery)
-    ];
-
     // Configure linear scale for poverty x axis
     var xLinearScale = d3.scaleLinear()
-        .domain(povertyLim)
-        .range([0, chartWidth]);
+        .range([0, chartWidth])
+        .domain(d3.extent(healthData, d => d.poverty));
+      
     
     // linear scake for y axis
     var yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(healthData, d => d.healthcare)])
-        .range([chartHeight, 0]);
+        .range([chartHeight, 0])
+        .domain([0, d3.max(healthData, d => d.noHealthInsurance)]);
 
     // create axes that use the previously defined scales
     var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axiisLeft(yLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
 
     // append axes to the chart
     chartGroup.append('g')
@@ -74,17 +59,19 @@ d3.csv('/data/data.csv', function(error, healthData) {
         .call(bottomAxis);
     
     chartGroup.append('g')
+        //.attr('transform', `translate(${margins.left}, 0)`)
         .call(leftAxis);
 
     // Create the circles for the scatter plot
-    var circlesGroup = chartGroup.selectAll('circle')
+    chartGroup.selectAll('circle')
         .data(healthData)
         .enter()
         .append('circle')
-        .attr('cx', d=>xLinearScale(d.poverty))
-        .attr('cy', d => yLinearScale(d.healthcare))
-        .attr('r', '30')
+        .attr('cx', d => xLinearScale(d.poverty))
+        .attr('cy', d => yLinearScale(d.noHealthInsurance))
+        .attr('r', '10')
         .attr('fill', 'blue')
-        .atter('opacity', '0.5');
+        .attr('opacity', '0.5')
+        .attr('text', d => d.abbr);
 
 });
